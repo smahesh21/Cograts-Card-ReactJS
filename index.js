cf0227a2663937ce84abe442103e94d67fa59258
Mahesh Srisailam <srisailammahesh77@gmail.com>
2022-03-10 09:14:14 +0000

    Congrats Card

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="main-heading">Congratulations</h1>
+    <div className="inner-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="heading">Kiran V</h1>
+      <p className="description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
